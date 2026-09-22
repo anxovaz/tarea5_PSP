f740b3e572accbac9df0084d4359451760958f74
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-22 10:07:58 +0200

    Agrego JavaDoc

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
 
+/**
+ * Clase Interfaz que pide datos al usuario
+ *
+ * @author Anxo Vázuqez
+ *
+ * @version 1.0
+ */
 public class Interfaz {
-    public static void main(String[] args){
-        while(true){
+    public static void main(String[] args) {
+        while (true) {
+            //Declaración escanner
             Scanner sc = new Scanner(System.in);
-            sc.useDelimiter("\n");
+            sc.useDelimiter("\n"); //por defecto es un epacio en vez de un Enter o salto de línea
 
             System.out.println("Introduce un número o escribe 'Salir': ");
             String str1 = sc.next();
-            if (str1.compareTo("Salir") == 0){
-                break;
+            if (str1.compareTo("Salir") == 0) {
+                break; //Sale del bucle y finaliza el programa
             }
-            if(str1.length() == 1){
-                String[] numeros = {"1","2","3","4","5","6","7","8","9","0"};
+
+            //Compruebo si es numérico
+            if (str1.length() == 1) {
+                String[] numeros = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
                 boolean esNumero = false;
-                for(int i = 0; i<numeros.length;i++){
-                    if(numeros[i].compareTo(str1) == 0){
+                for (int i = 0; i < numeros.length; i++) {
+                    if (numeros[i].compareTo(str1) == 0) {
                         esNumero = true;
                         break;
                     }
                 }
-
-                if (esNumero){
+                if (esNumero) {
                     break;
+                } else {
+                    System.out.println("Valor incorrecto, vuelve a intentarlo");
                 }
             }
-            System.out.println("Valor incorrecto, vuelve a intentarlo");
 
 
         }
